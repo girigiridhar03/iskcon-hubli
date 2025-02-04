0ecfa069862f2844aa8feae39a18f0be7b004620
Balaji-C-V <balaji.cv.1995@gmail.com>
2025-02-05 00:37:32 +0530

    Update AdminCreateCampaign.jsx

--- src/Components/AdminPageComponents/AdminCreateCampaign.jsx
+++ src/Components/AdminPageComponents/AdminCreateCampaign.jsx
@@ -34,7 +34,7 @@ const AdminCreateCampaign = () => {
       alert("Please fill in all fields.");
       return;
     }
-
+    console.log(JSON.strigify(formData));
     dispatch(adminCreateCampaign(formData));
 
     return setFormData({
